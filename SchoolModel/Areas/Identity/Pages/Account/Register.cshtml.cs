c757ba0a144d7f72983a2ef2959351d5b1f64d5e
NicolarsIsrael <bakareisrael@gmail.com>
2019-10-22 17:18:49 +0100

    App user

--- SchoolModel/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ SchoolModel/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using SchoolModel.Core;
 
 namespace SchoolModel.Areas.Identity.Pages.Account
 {
@@ -69,7 +70,7 @@ namespace SchoolModel.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
